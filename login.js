cfde777dc78a29fa2b6e11601425451b9e04048b
devjoyboy <devjoyboy@gmail.com>
2022-04-29 01:49:46 -0300

    Atualizacao de login

--- login.js
+++ login.js
@@ -1,3 +1,10 @@
 //Trabalhando no login
 
 //Finalizando sistema de login
+
+function login(nome, email){
+    return 'Bem vindo!'
+}
+
+// Sistema de login completo
+// Retornando o nome do usuário quando logado
